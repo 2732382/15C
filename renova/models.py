efa181d1b4e4f4d54c0a2e750dd2db316ae5290f
Ben Whitehead (student) <2732382W@student.gla.ac.uk>
2024-03-01 17:43:34 +0000

    first structural edit

--- renova/models.py
+++ renova/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    pass
+
+class Group(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Log(models.Model):
+    pass
