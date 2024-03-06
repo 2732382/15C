cf0815231c589c8b1818911765e765eeb22d6fa2
Michal Cwiklinski <2778904C@student.gla.ac.uk>
2024-03-06 15:14:49 +0000

    initial base.html. TEMPLATE_DIR appended in settings. Added Renova.jpg

--- group_project/settings.py
+++ group_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'group_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
