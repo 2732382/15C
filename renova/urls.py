021388708cd5be311e3f25e683f94e79767d9f88
“Ronit <2651435s@student.gla.ac.uk>
2024-03-08 17:10:35 +0000

    Views & URLs

--- renova/urls.py
+++ renova/urls.py
@@ -4,5 +4,15 @@ from renova import views
 app_name = "renova"
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('faq/', views.faq, name='faq'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('my-logs/', views.my_logs, name='my_logs'),
+    path('my-logs/record-log/', views.record_log, name='record_log'),
+    path('my-account/', views.my_account, name='my_account'),
+    path('groups/', views.groups, name='groups'),
+    path('groups/<slug:group_name_slug>/', views.group_detail, name='group_detail'),
+    path('groups/make-group/', views.make_group, name='make_group'),
 ]
--- renova/views.py
+++ renova/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
+from .models import User, Group 
 
 def index(request):
     return render(request, 'renova/index.html')
+
+def login_view(request):
+
+    return render(request, 'login.html')
+
+def register(request):
+
+    return render(request, 'register.html')
+
+def faq(request):
+
+    return render(request, 'faq.html')
+
+def about_us(request):
+
+    return render(request, 'about_us.html')
+
+def my_logs(request):
+
+    return render(request, 'my_logs.html')
+
+def record_log(request):
+
+    return render(request, 'record_log.html')
+
+def my_account(request):
+
+    return render(request, 'my_account.html')
+
+def groups(request):
+    
+    return render(request, 'groups.html')
+
+def group_detail(request, group_name_slug):
+    
+    return render(request, 'group_detail.html', {'group_name_slug': group_name_slug})
+
+def make_group(request):
+    
+    return render(request, 'make_group.html')
