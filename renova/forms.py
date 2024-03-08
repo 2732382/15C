7f016ece6d3924b747fe4a3f3a1bc67b3e6c3a76
Ben Whitehead <2732382W@student.gla.ac.uk>
2024-03-08 03:12:17 +0000

    Ben 1.1 UPro registered + forms initial

---
+++ renova/forms.py
@@ -0,0 +1,15 @@
+#It's platonic in here
+from django import forms
+from renova.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('picture',)
--- renova/models.py
+++ renova/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class UserProfile(models.Model):
-    user = models.OnToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     picture = models.ImageField(upload_to='profile_images', blank = True)
 
