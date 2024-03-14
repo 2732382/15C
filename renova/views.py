3646884a057cc7f2003c9bbac2efeb7812a0304e
Michal Cwiklinski <2778904C@student.gla.ac.uk>
2024-03-14 22:32:56 +0000

    fixed main links - matched to renova subdirectory

--- renova/views.py
+++ renova/views.py
@@ -5,11 +5,9 @@ def index(request):
     return render(request, 'renova/index.html')
 
 def login(request):
-
-    return render(request, 'login.html')
+    return render(request, 'registration/login.html')
 
 def register(request):
-
     user_form = UserForm()
     profile_form = UserProfileForm()
 
@@ -17,37 +15,28 @@ def register(request):
         'user_form': user_form,
         'profile_form': profile_form
     }
-    
     return render(request, 'registration/register.html', context_dict)
 
 def faq(request):
-
-    return render(request, 'faq.html')
+    return render(request, 'renova/faq.html')
 
 def about_us(request):
-
-    return render(request, 'about_us.html')
+    return render(request, 'renova/about_us.html')
 
 def my_logs(request):
-
-    return render(request, 'my_logs.html')
+    return render(request, 'renova/my_logs.html')
 
 def record_log(request):
-
-    return render(request, 'record_log.html')
+    return render(request, 'renova/record_log.html')
 
 def my_account(request):
-
-    return render(request, 'my_account.html')
+    return render(request, 'renova/my_account.html')
 
 def groups(request):
-    
-    return render(request, 'groups.html')
+    return render(request, 'renova/groups.html')
 
 def group_detail(request, group_name_slug):
-    
     return render(request, 'group_detail.html', {'group_name_slug': group_name_slug})
 
 def make_group(request):
-    
-    return render(request, 'make_group.html')
+    return render(request, 'renova/make_group.html')
