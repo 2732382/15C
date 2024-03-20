bb833fc8b2d8190a2bb3c69cbee37302e2c7942a
Ben Whitehead <2732382W@student.gla.ac.uk>
2024-03-20 19:15:30 +0000

    Group view tests

--- renova/tests.py
+++ renova/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 from renova.models import *
 
-class BasicViewTests(TestCase):
+class ViewTests(TestCase):
 
     def test_index_view(self):
         response = self.client.get(reverse('renova:index'))
@@ -29,13 +29,36 @@ class BasicViewTests(TestCase):
         response = self.client.get(reverse('renova:my_account'))
         self.assertEqual(response.status_code, 302)
 
-    def test_groups_view(self):
-        response = self.client.get(reverse('renova:groups'))
-        self.assertEqual(response.status_code, 200)
-    
-    def test_make_group_view(self):
+    def test_make_group_view_no_login(self):
         response = self.client.get(reverse('renova:make_group'))
         self.assertEqual(response.status_code, 302)
 
-class GroupModelTests(TestCase):
-    pass
+class GroupsViewTests(TestCase):
+
+    def test_groups_view(self):
+        response = self.client.get(reverse('renova:groups'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no groups present.")
+        self.assertContains(response, "There are no new groups present.")
+    
+    def test_groups_view_with_content(self):
+        example_admin = User.objects.get_or_create(username="temp_admin")[0]
+        running_group = Group.objects.get_or_create(name="Running Example")[0]
+        running_group.admin = example_admin
+        running_group.save()
+        
+        swimming_group = Group.objects.get_or_create(name="Swimming Example")[0]
+        swimming_group.admin = example_admin
+        swimming_group.save()
+
+        
+        response = self.client.get(reverse('renova:groups'))
+
+        self.assertContains(response, "Running Example")
+        self.assertContains(response, "Swimming Example")
+
+        num_popular_groups = len(response.context['popular_groups'])
+        num_recent_groups = len(response.context['recent_groups'])
+        self.assertEquals(num_popular_groups, 2)
+        self.assertEquals(num_recent_groups, 2)
