c888af97f52340100fe28e4012f94133e22151d1
losmahoma <2840440a@student.gla.ac.uk>
2024-03-20 08:09:47 +0000

    added to record_log.js

--- static/javascript/record_log.js
+++ static/javascript/record_log.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-    $(".log-form").on("submit", function(event){
-        
+    $("LogForm").on("submit", function(event){
+        event.preventDefault(); // Prevent default form submission
+
+        var formData = {
+            'water': $('#id_water').val(),
+            'calories': $('#id_calories').val(),
+            'sleep': $('#id_sleep').val(),
+            'activity_name': $('#activity_name').val(),
+            'activity_duration': $('#activity_duration').val(),
+            'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/renova/my-logs/", // URL to your Django view
+            data: formData,
+            success: function(response) {
+                console.log("Log successfully recorded.");
+                // Optionally, redirect to my_logs page or update the UI accordingly
+            },
+            error: function(response) {
+                console.log("Error recording log.");
+            }
+        });
     });
 });
