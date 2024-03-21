bef5f2cf20f4a047f00ea2408dd2fa8b137fd01f
Ben Whitehead (student) <2732382W@student.gla.ac.uk>
2024-03-21 18:52:43 +0000

    Overall dial complete

--- static/javascript/my_logs.js
+++ static/javascript/my_logs.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     var percent_sleep = 100*(total_sleep/target_sleep)
     var percent_duration = 100*(total_duration/target_duration)
 
+    var average = (percent_calories+percent_water+percent_sleep+percent_duration)/4
+
     $("#calorie-dial").css({
         background: "conic-gradient(red 0%, rgb(255, 123, 0) "+percent_calories+"%, transparent "+percent_calories+"%, lightgrey 100%)",
     });
@@ -17,4 +19,9 @@ $(document).ready(function() {
     $("#duration-dial").css({
         background: "conic-gradient(rgb(255, 166, 0) 0%, rgb(236, 228, 109) "+percent_duration+"%, transparent "+percent_duration+"%, lightgrey 100%)",
     });
+
+    $("#overall-dial").css({
+        background: "conic-gradient(rgb(0, 143, 83) 0%, rgb(0, 255, 136) "+average+"%, transparent "+average+"%, lightgrey 100%)",
+    });
+    $("#overall-dial .dial-text strong").text(average);
 });
