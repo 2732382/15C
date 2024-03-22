711904adaac84aa7c74184ec25b7478218151f17
Ben Whitehead <2732382W@student.gla.ac.uk>
2024-03-22 15:50:05 +0000

    Populate users and comments, inc. small fixes

--- populate_renova.py
+++ populate_renova.py
@@ -9,10 +9,14 @@ from renova.models import *
 def populate():
 
     first_admin = User.objects.get_or_create(username="temp_admin", password="12345")[0]
+    first_admin.save()
 
-    RB_user = User.objects.get_or_create(username='RedBlue',password='Conglomerate123')
-    OG_user = User.objects.get_or_create(username='OrangeGrey',password='Building123')
-    YG_user = User.objects.get_or_create(username='YellowGreen',password='Representative123')
+    RB_user = User.objects.get_or_create(username='RedBlue',password='Conglomerate123')[0]
+    RB_user.save()
+    OG_user = User.objects.get_or_create(username='OrangeGrey',password='Building123')[0]
+    OG_user.save()
+    YG_user = User.objects.get_or_create(username='YellowGreen',password='Representative123')[0]
+    YG_user.save()
 
     groups = {
         'Running group': {"admin": first_admin,
@@ -25,14 +29,34 @@ def populate():
                            "announcements": "TCA new set session tomorrow."}
     }
 
+    comments = {
+        'Running group': [
+            {"user":RB_user, "text":"I love running"},
+            {"user":OG_user, "text":"Example comment"}
+        ],
+        'Climbing group': [
+            {"user":YG_user, "text":"I love climbing"},
+            {"user":OG_user, "text":"Another example comment"}
+        ],
+    }
+
     for group,group_data in groups.items():
         g = Group.objects.get_or_create(name=group,
                                         admin=group_data["admin"],
                                         description=group_data["description"],
                                         announcements=group_data["announcements"])[0]
+        g.save()
+        
         for member in group_data["members"]:
             g.members.add(member)
-        g.save()
+        
+
+        for comm in comments[group]:
+            c =Comment.objects.get_or_create(group=g,
+                                             user=comm["user"],
+                                             text=comm["text"])[0]
+            c.save()
+        
 
     
 
